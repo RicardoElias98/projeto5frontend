9cba171fab3b983ec0b3de4bd56df52a81b48859
ricardoelias98 <uc2016225298@student.uc.pt>
2024-04-09 21:29:25 +0100

    botão de lida
    biblioteca react icons

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { userStore } from "../stores/UserStore";
+import { FaEye } from "react-icons/fa";
 
-function Message({ text, checked, sender}) {
-  const messageClass = checked ? "message-checked" : "message-unchecked";
+function Message({ text, checked, sender }) {
+  const [isMessageRead, setIsMessageRead] = useState(checked);
   const loginUser = userStore((state) => state.loginUser);
+
+  const messageClass = isMessageRead ? "message-checked" : "message-unchecked";
+
+  const handleEyeClick = () => {
+    // Lógica para marcar a mensagem como lida
+    setIsMessageRead(true);
+  };
 
   if (sender === loginUser.username) {
     return (
       <div className={"message-container-right"}>
-        <div className={`message-bubble ${messageClass}`}>{text}</div>
+        <div className={`message-bubble ${messageClass}`}>
+          {text}
+        </div>
       </div>
     );
   } else {
     return (
       <div className={"message-container-left"}>
-        <div className={`message-bubble ${messageClass}`}>{text}</div>
+        <div className={`message-bubble ${messageClass}`}>
+          {text}
+          {!isMessageRead && (
+            <button onClick={handleEyeClick}>
+              <FaEye />
+            </button>
+          )}
+        </div>
       </div>
     );
   }
 }
 
 export default Message;
+
