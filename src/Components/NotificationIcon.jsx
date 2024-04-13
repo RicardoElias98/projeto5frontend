0dbac35f40913b342b9aed8a0ad6ae99c0b15bba
ricardoelias98 <uc2016225298@student.uc.pt>
2024-04-13 17:17:25 +0100

    abrir notificações

--- src/Components/NotificationIcon.jsx
+++ src/Components/NotificationIcon.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaBell } from 'react-icons/fa';
 
 const NotificationIcon = ({ count }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNotificationWindow = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <FaBell size={24} color="gray" />
+      <FaBell size={24} color="gray" onClick={toggleNotificationWindow} />
       {count > 0 && (
         <span className="notification-count">{count}</span>
+      )}
+      {isOpen && (
+        <div className="notification-window">
+          {/* Aqui você pode renderizar o conteúdo das notificações */}
+          <p>Conteúdo das notificações...</p>
+        </div>
       )}
     </div>
   );
 };
 
 export default NotificationIcon;
+
