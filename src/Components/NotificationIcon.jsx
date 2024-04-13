0472508349daa909a6bd3bf30e03d3dd2b11e0e8
ricardoelias98 <uc2016225298@student.uc.pt>
2024-04-13 19:43:14 +0100

    notificacoes vistas ao abrir

--- src/Components/NotificationIcon.jsx
+++ src/Components/NotificationIcon.jsx
@@ -6,9 +6,24 @@ import NotificationMsg from "./NotificationMsg";
 const NotificationIcon = ({ count }) => {
   const [isOpen, setIsOpen] = useState(false);
   const notification = userStore((state) => state.notification);
+  const token = userStore((state) => state.token);
 
   const toggleNotificationWindow = () => {
-    setIsOpen(!isOpen);
+    setIsOpen(true);
+    fetch("http://localhost:8080/projecto5backend/rest/user/checkNotification", {
+      method: "PUT",
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        token: token,
+      },
+    }).then((response) => {
+      if (response.status === 403) {
+        alert("User with this token is not found");
+      } else if (response.status === 200) {
+        console.log("Notifications are checked");
+      }
+    });
   };
 
   return (
