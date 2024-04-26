f35c210d5174ff47d677600c9ad8a6e35e850133
ricardoelias98 <uc2016225298@student.uc.pt>
2024-04-26 15:57:39 +0100

    cruz nas notificações e a aparecerem certo

--- src/Components/NotificationIcon.jsx
+++ src/Components/NotificationIcon.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { FaBell } from "react-icons/fa";
+import { FaBell, FaTimes } from "react-icons/fa";
 import { userStore } from "../stores/UserStore";
 import NotificationMsg from "./NotificationMsg";
 import { useNavigate } from "react-router-dom";
 import { useRef } from "react";
 import { useEffect } from "react";
-
 
 const NotificationIcon = ({ count }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,8 +15,34 @@ const NotificationIcon = ({ count }) => {
   const loginUser = userStore((state) => state.loginUser);
   const username = loginUser.username;
   const updateNotification = userStore((state) => state.updateNotification);
-  const updateNotCheckedNotification = userStore((state) => state.updateNotCheckedNotification);
+  const updateNotCheckedNotification = userStore(
+    (state) => state.updateNotCheckedNotification
+  );
 
+  const closeNotificationWindow = () => {
+    fetch(
+      "http://localhost:8080/projecto5backend/rest/notif/checkNotification",
+      {
+        method: "PUT",
+        headers: {
+          Accept: "*/*",
+          "Content-Type": "application/json",
+          token: token,
+        },
+      }
+    ).then((response) => {
+      if (response.status === 403) {
+        alert("User with this token is not found");
+      } else if (response.status === 200) {
+        console.log("Notifications are checked");
+        updateNotCheckedNotification([]);
+      } else if (response.status === 401) {
+        alert("Token timer expired, please login again.");
+        navigate("/goBackInitialPage", { replace: true });
+      }
+    });
+    setIsOpen(false);
+  }
 
   const toggleNotificationWindow = () => {
     setIsOpen(true);
@@ -39,37 +64,16 @@ const NotificationIcon = ({ count }) => {
       } else if (response.status === 401) {
         alert("Token timer expired, please login again.");
         navigate("/goBackInitialPage", { replace: true });
-      } 
-    }
-    );
-    fetch("http://localhost:8080/projecto5backend/rest/notif/checkNotification", {
-      method: "PUT",
-      headers: {
-        Accept: "*/*",
-        "Content-Type": "application/json",
-        token: token,
-      },
-    }).then((response) => {
-      if (response.status === 403) {
-        alert("User with this token is not found");
-      } else if (response.status === 200) {
-        console.log("Notifications are checked");
-        updateNotCheckedNotification([]);
-      } 
-      else if (response.status === 401) {
-        alert("Token timer expired, please login again.");
-        navigate("/goBackInitialPage", { replace: true });
-      } 
+      }
     });
   };
 
   useEffect(() => {
     if (isOpen) {
-      notificationWindowRef.current.scrollTop = notificationWindowRef.current.scrollHeight;
+      notificationWindowRef.current.scrollTop =
+        notificationWindowRef.current.scrollHeight;
     }
-
   }, [notification, isOpen]);
-
 
   return (
     <div>
@@ -77,12 +81,31 @@ const NotificationIcon = ({ count }) => {
       {count > 0 && <span className="notification-count">{count}</span>}
       {isOpen && (
         <div className="notification-window" ref={notificationWindowRef}>
+           <FaTimes className="close-icon" onClick={closeNotificationWindow} />
           {notification.length === 0 ? (
             <p>No notifications</p>
           ) : (
-            notification.map((ntf) => (
-              <NotificationMsg key={ntf.id} text={ntf.text} checked={ntf.checked} />
-            ))
+            <>
+              {notification
+                .filter((ntf) => ntf.checked)
+                .slice(-5)
+                .map((ntf) => (
+                  <NotificationMsg
+                    key={ntf.id}
+                    text={ntf.text}
+                    checked={ntf.checked}
+                  />
+                ))}
+                 {notification
+                .filter((ntf) => !ntf.checked)
+                .map((ntf) => (
+                  <NotificationMsg
+                    key={ntf.id}
+                    text={ntf.text}
+                    checked={ntf.checked}
+                  />
+                ))}
+            </>
           )}
         </div>
       )}
